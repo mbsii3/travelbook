90178ec94958add0f7574de79bc4393f1d19f8c2
mbsii3 <mshortjr3@gmail.com>
2023-02-24 04:30:07 -0600

    successfully added oauth

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 const indexRouter = require('./routes/index');
 const destinationsRouter = require('./routes/destinations');
@@ -22,6 +25,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/destinations', destinationsRouter);
